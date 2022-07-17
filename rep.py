331938638e28c93b806e417e85425b6da72d9655
ipasforyou <109419865+ipasforyou@users.noreply.github.com>
2022-07-17 22:07:03 +0530

    Update rep.py

--- rep.py
+++ rep.py
@@ -3,7 +3,7 @@ import sys
 search_text = "DUMMY"
 replace_text = sys.argv[1]
 
-with open(dummy.plist', 'r') as file:
+with open('dummy.plist', 'r') as file:
     data = file.read()
     data = data.replace(search_text, replace_text)
 with open(r'dummy.plist', 'w') as file:
